315372a25c70219f4c605eac323bed503f7bb8ba
Kaden Thielmann <167242698+KadenThielmann@users.noreply.github.com>
2026-05-12 10:46:11 -0400

    Update word bank

--- laurdle/words.js
+++ laurdle/words.js
@@ -1,17 +1,17 @@
 export const WORD_BANK = [
-    { word: "CLIMB", message: "Our first activity together. You stepped out of your comfort zone to join me in climbing, which I really appreciated." },
-    { word: "ESCAPE", message: "I'm still counting our escape as a success!" },
+    { word: "ESCAPE", message: "I'm still counting our escape as a success. You were impressive with how quickly you solved some of those puzzles!" },
+    { word: "FARKLE", message: "The inspiration for this site. I remember you mentioning you play this on your phone occasionally." },
+    { word: "SCONE", message: "A throwback to our first date." },
+    { word: "BURGER", message: "Our first meal together. We were the last two to ever get that Farm Boy deal." },
+    { word: "BIBLE", message: "I appreciate how naturally your faith shows up in your life. It's one of the things I admire most about you." },
+    { word: "CLIMB", message: "Our first activity together. I really appreciated you stepping out of your comfort zone to join me." },
+    { word: "CHERRY", message: "Official verdict of fresh cherry pancakes: Pending. I'm looking forward to making them with you. 🍒" },
     { word: "FUDGE", message: "Car picnic with your favourite cake!" },
     { word: "BRIDGE", message: "We walked through it 4 weeks after you said we'd have to come back to it sometime." },
     { word: "BROWN", message: "apparently kindergarten Kaden's favourite colour" },
-    { word: "SCONE", message: "throwback to our first date" },
     { word: "BOARD", message: "You put up a good fight at The Round Table." },
-    { word: "FARKLE", message: "The game that inspired this website. You mentioned you play Farkle occasionally on your phone browser." },
     { word: "CHALK", message: "I found chalk on everything after our climbing date" },
-    { word: "BURGER", message: "Our first meal together. The last two to ever get that Farm Boy deal" },
     { word: "SHELL", message: "Or should it be Shell x2? 😉" },
-    { word: "CHERRY", message: "Future official verdict of fresh cherry pancakes pending 🍒" },
     { word: "WINDOW", message: "A necessity over our kitchen sink." },
-    { word: "VACUUM", message: "We'll make this a blue job 😉" },
-    { word: "BIBLE", message: "I appreciate how naturally faith shows up in your life" }
+    { word: "VACUUM", message: "We'll make this a blue job 😉" }
 ];
