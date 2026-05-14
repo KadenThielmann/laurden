e596f0c08162f62e9ce8c6424237b505c5843311
Kaden Thielmann <167242698+KadenThielmann@users.noreply.github.com>
2026-05-13 23:52:50 -0400

    Update word order

--- laurdle/words.js
+++ laurdle/words.js
@@ -1,11 +1,11 @@
 export const WORD_BANK = [
     { word: "ESCAPE", message: "I'm still counting our escape as a success. You were impressive with how quickly you solved some of those puzzles!" },
+    { word: "BIBLE", message: "I appreciate how naturally your faith shows up in your life. It's one of the things I admire most about you." },
+    { word: "CHERRY", message: "Official verdict of fresh cherry pancakes: Pending. I'm looking forward to making them with you. 🍒" },
     { word: "FARKLE", message: "The inspiration for this site. I remember you mentioning you play this on your phone occasionally." },
     { word: "SCONE", message: "A throwback to our first date." },
     { word: "BURGER", message: "Our first meal together. We were the last two to ever get that Farm Boy deal." },
-    { word: "BIBLE", message: "I appreciate how naturally your faith shows up in your life. It's one of the things I admire most about you." },
     { word: "CLIMB", message: "Our first activity together. I really appreciated you stepping out of your comfort zone to join me." },
-    { word: "CHERRY", message: "Official verdict of fresh cherry pancakes: Pending. I'm looking forward to making them with you. 🍒" },
     { word: "FUDGE", message: "Car picnic with your favourite cake!" },
     { word: "BRIDGE", message: "We walked through it 4 weeks after you said we'd have to come back to it sometime." },
     { word: "BROWN", message: "Apparently kindergarten Kaden's favourite colour" },
